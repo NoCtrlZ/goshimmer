2f2d361eaa02c92bc5399377ffbe71a81324e585
lunfardo314 <evaldas.drasutis@iota.org>
2020-02-20 13:09:11 +0200

    docker config

--- plugins/qnode/start4.bat
+++ plugins/qnode/start4.bat
@@ -1,8 +1,8 @@
 cd C:\Users\evaldas\Documents\proj\Go\src\github.com\lunfardo314\goshimmer
 go install
-cd C:\Users\evaldas\Documents\proj\Go\src\github.com\lunfardo314\goshimmer\plugins\qnode
-start startgs.bat 0
-start startgs.bat 1
-start startgs.bat 2
-start startgs.bat 3
-sendepoch
+cd C:\Users\evaldas\Documents\proj\Go\src\github.com\lunfardo314\goshimmer
+start plugins\qnode\startgs.bat 0
+start plugins\qnode\startgs.bat 1
+start plugins\qnode\startgs.bat 2
+start plugins\qnode\startgs.bat 3
+
--- plugins/qnode/startgs.bat
+++ plugins/qnode/startgs.bat
@@ -1,2 +1,2 @@
-goshimmer -database-directory C:\Users\evaldas\Documents\proj\site_data\goshimmer\%1 -webapi-port 808%1 -qnode-udp-port 400%1
+goshimmer --database.directory C:\Users\evaldas\Documents\proj\site_data\goshimmer\%1 --webapi.bindAddress 127.0.0.1:808%1 --qnode.udpPort 400%1
 pause
