ae612df14c815aeba890d989e11083f0de1989f1
NoCtrlZ <phantomofrotten@gmail.com>
2020-04-21 21:08:06 +0900

    build success

--- plugins/qnode/api/dkgapi/get.go
+++ plugins/qnode/api/dkgapi/get.go
@@ -3,6 +3,7 @@ package dkgapi
 import (
 	"github.com/iotaledger/goshimmer/plugins/qnode/api/utils"
 	"github.com/iotaledger/goshimmer/plugins/qnode/registry"
+	"github.com/iotaledger/goshimmer/plugins/qnode/tcrypto"
 	"github.com/labstack/echo"
 	"net/http"
 )
@@ -16,7 +17,11 @@ type GetDKSResponse struct {
 	Err  string             `json:"err"`
 }
 
-func HandlerGetDks(c echo.Context) GetDKSResponse {
+func HandlerGetDks(c echo.Context) error {
+	return utils.ToJSON(c, http.StatusOK, GetDKSSetReq())
+}
+
+func GetDKSSetReq() *GetDKSResponse {
 	dkslist, ok := registry.GetAllDKShare()
 	if !ok {
 		return &GetDKSResponse{Err: "key set already exist"}
